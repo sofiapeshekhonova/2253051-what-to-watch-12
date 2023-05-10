811cf399043136c55de090ce536526e4b97cc551
Sofia Peshekhonova <Peshehonova94@yandex.ru>
2023-05-10 17:23:40 +0300

    fix review

--- project/src/components/reviews/reviews.tsx
+++ project/src/components/reviews/reviews.tsx
@@ -3,10 +3,18 @@ import Review from '../review/review';
 
 function Reviews() {
   const reviews = useAppSelector((state) => state.review);
+  const even = reviews.filter((rev)=> rev.id % 2 === 0);
+  const odd = reviews.filter((rev)=> rev.id % 2 !== 0);
+
   return (
     <div className="film-card__reviews film-card__row">
       <div className="film-card__reviews-col">
-        {reviews.map((review) => (
+        {odd.map((review) => (
+          <Review review={review} key={review.id}/>
+        ))}
+      </div>
+      <div className="film-card__reviews-col">
+        {even.map((review) => (
           <Review review={review} key={review.id}/>
         ))}
       </div>
