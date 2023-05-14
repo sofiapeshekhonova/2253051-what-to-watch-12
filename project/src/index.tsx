500a32e3bcf0baf811440e95332f2227df6152ab
Sofia Peshekhonova <Peshehonova94@yandex.ru>
2023-05-14 11:33:28 +0300

    tostify, icon

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 import { Provider } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
 import { store } from './store';
 import { checkAuthAction, fetchFavoritesMoviesAction, fetchMoviesAction, fetchPromoMovieAction } from './store/api-actions';
+import 'react-toastify/dist/ReactToastify.css';
 
 store.dispatch(fetchMoviesAction());
 store.dispatch(fetchPromoMovieAction());
@@ -17,6 +19,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <ToastContainer />
       <App />
     </Provider>
   </React.StrictMode>,
--- project/src/services/api.ts
+++ project/src/services/api.ts
@@ -1,8 +1,14 @@
-import axios, {AxiosInstance, AxiosRequestConfig} from 'axios';
-import {getToken} from './token';
+import axios, { AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
+import { toast } from 'react-toastify';
+import { getToken } from './token';
+import { StatusCodes } from 'http-status-codes';
 
 const BACKEND_URL = 'https://12.react.pages.academy/wtw';
 const REQUEST_TIMEOUT = 5000;
+
+const StatusCodeMapping = [StatusCodes.BAD_REQUEST, StatusCodes.UNAUTHORIZED, StatusCodes.NOT_FOUND];
+
+const shouldDisplayError = (response: AxiosResponse) => !!StatusCodeMapping.includes(response.status);
 
 export const createAPI = (): AxiosInstance => {
   const api = axios.create({
@@ -13,13 +19,21 @@ export const createAPI = (): AxiosInstance => {
   api.interceptors.request.use(
     (config: AxiosRequestConfig) => {
       const token = getToken();
-
       if (token && config.headers) {
         config.headers['x-token'] = token;
       }
-
       return config;
     },
+  );
+
+  api.interceptors.response.use(
+    (response) => response,
+    (error: AxiosError<{ error: string }>) => {
+      if (error.response && shouldDisplayError(error.response)) {
+        toast.warn(error.response.data.error);
+      }
+      throw error;
+    }
   );
 
   return api;
